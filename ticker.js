ecfd04a6d06016c84f1541853d162e21f95eb584
DekaShichi <dekashichi@live.com>
2014-12-21 00:32:52 -0700

    Added (body) volume to ticker.js

--- ticker.js
+++ ticker.js
@@ -12,6 +12,7 @@ var lengthItems = [
 	{name:"pencil", plural:"pencils",                           value:"1.905e-1"},
 	{name:"13lb bowling ball", plural:"13lb bowling balls",     value:"2.159e-1"},
 	{name:"human", plural:"humans",                             value:"1.778e0"},
+	{name:"midsize car", plural:"midsize cars",                 value:"5.11302e0"},
 	{name:"Eiffel Tower", plural:"Eiffel Towers",               value:"3.01e+2"},
 	{name:"Burj Khalifa", plural:"Burj Khalifas",               value:"8.298e+2"},
 	// Both in special-case prominence and from sea level
@@ -40,21 +41,74 @@ var lengthItems = [
 	{name:"observable universe", plural:"observable universes", value:"8.639897228e+26"}
 ];
 
+// In cubic meters
+var volumeItems = [
+	{name:"hydrogen atom", plural:"hydrogen atoms",            value:"7.23e-30"},
+	{name:"13lb bowling ball", plural:"13lb bowling balls",    value:"4.215475177e-2"},
+	{name:"human", plural:"humans",                            value:"7.1e-2"},
+	// Based on 2014 Chevrolet Impala http://www.cars.com/chevrolet/impala/2014/specifications
+	// Assumed Exterior Length * Height * Width / 2 (passenger volume: ~2.97327 m^3)
+	// Length: 5.11302 m (201.3")
+	// Width: 1.8542 m (73.0")
+	// Height: 1.49606 m (58.9")
+	{name:"midsize car", plural:"midsize cars",                 value:"7.091744555e0"},
+	// Calculated based on lwh/3, where the base is 125 m * 125 m, and h = 324 m
+	{name:"Eiffel Tower", plural:"Eiffel Towers",               value:"1.6875e+6"},
+	{name:"Moon", plural:"Moons",                               value:"2.1958e+19"},
+	{name:"Mercury", plural:"Mercuries",                        value:"6.083e+19"},
+	{name:"Mars", plural:"Marses",                              value:"1.6318e+20"},
+	{name:"Venus", plural:"Venuses",                            value:"9.28e+20"},
+	{name:"Earth", plural:"Earths",                             value:"1.08321e+21"},
+	{name:"Neptune", plural:"Neptunes",                         value:"6.254e+22"},
+	{name:"Uranus", plural:"Uranuses",                          value:"6.833e+22"},
+	{name:"Saturn", plural:"Saturns",                           value:"8.2713e+23"},
+	{name:"Jupiter", plural:"Jupiters",                         value:"1.4313e+24"},
+	{name:"Sun", plural:"Suns",                                 value:"1.41e+27"},
+	{name:"Solar System", plural:"Solar Systems",               value:"1.402377772e+40"},
+	{name:"Milky Way Galaxy", plural:"Milky Way Galaxies",      value:"3.3e+61"},
+	{name:"observable universe", plural:"observable universes", value:"3.4e+80"}
+]
+
 function getNextTicker(person) {
 	var tickerText = "Your character is as ";
-	switch(Math.floor(Math.random() * 1)) {
+	var DEFAULT_TEXT = tickerText;
+	switch(Math.floor(Math.random() * 2)) {
 		// Height
 		case 0:
-			var index = Math.floor(Math.random() * lengthItems.length);
-			var item = lengthItems[index];
-			var value = Math.floor(person.height / item.value);
-			tickerText += "tall as " + value + " ";
-			if(value == 1) {
-				tickerText += item.name;
+			do {
+				tickerText = DEFAULT_TEXT;
+				var index = Math.floor(Math.random() * lengthItems.length);
+				var item = lengthItems[index];
+				var value = Math.floor(person.height / item.value);
+				tickerText += "tall as " + value + " ";
+				if(value == 1) {
+					tickerText += item.name;
+				}
+				else {
+					tickerText += item.plural;
+				}
 			}
-			else {
-				tickerText += item.plural;
+			// If index is 0, exit, otherwise may cause infinite-loop.
+			while(value <= 0 && index > 0);
+			break;
+		// Volume
+		case 1:
+			do {
+				tickerText = DEFAULT_TEXT;
+				var index = Math.floor(Math.random() * volumeItems.length);
+				var item = volumeItems[index];
+				var value = Math.floor(person.volume / item.value);
+				tickerText += "big as " + value + " ";
+				if(value == 1) {
+					tickerText += item.name;
+				}
+				else {
+					tickerText += item.plural;
+				}
 			}
+			// If index is 0, exit, otherwise may cause infinite-loop.
+			while(value <= 0 && index > 0);
+			break;
 	}
 	tickerText += "!";
 
